2a7b6c48f61ed6bb6fe0e8ea82b8a6b3cdcbc466
Adam Kossakowski <adam.kossakowski@outlook.com>
2024-02-04 13:19:19 +0100

    feature branch-1.1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I was changed! Feature branch 1.1
         </a>
       </header>
     </div>
